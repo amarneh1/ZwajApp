25c3d870ed7d6b0825fffba89e678dc8fae651e2
Tawfiq Alamarneh <amarneh1@hotmail.com>
2023-11-20 17:17:18 +0400

    Initial Commit

---
+++ ZwajApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZwajApp.API.Data;
+
+namespace ZwajApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+            
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var Values = await _context.Values.ToListAsync();
+            return Ok(Values);
+            // return new string[] { "value1", "value2" ,"value2"};
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var Value =await _context.Values.FirstOrDefaultAsync(x => x.id==id);
+            return Ok(Value);
+            // return $"value{id}";
+            
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
